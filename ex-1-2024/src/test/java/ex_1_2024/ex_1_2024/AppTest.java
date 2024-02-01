6e3c9480dca8325b044881c9171e52b337e0ea1b
Layan Gr <layanaagrifat@gmail.com>
2024-02-01 19:45:09 +0200

    exe-1

---
+++ ex-1-2024/src/test/java/ex_1_2024/ex_1_2024/AppTest.java
@@ -0,0 +1,20 @@
+package ex_1_2024.ex_1_2024;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
